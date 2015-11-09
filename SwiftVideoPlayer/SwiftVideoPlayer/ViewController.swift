ef9654f1cef2b2469c8660e314a6dcb317833059
PrashantKumar Mangukiya <pbMangukiya@gmail.com>
2015-11-09 20:05:40 +0530

    Update source - ( source not in working state)

--- SwiftVideoPlayer/SwiftVideoPlayer/ViewController.swift
+++ SwiftVideoPlayer/SwiftVideoPlayer/ViewController.swift
@@ -15,6 +15,15 @@ class ViewController: UIViewController, AVPlayerViewControllerDelegate {
 
     
     
+    // action - choose video button
+    @IBAction func chooseVideoButtonAction(sender: UIButton) {
+        self.performSegueWithIdentifier("seguePlayVideo", sender: self)
+    }
+    
+    
+    
+    
+    // MARK: - View functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,14 +34,24 @@ class ViewController: UIViewController, AVPlayerViewControllerDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
     
+    
+    
+    
+    // MARK: - Navigation function
+
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
 
         if segue.identifier == "seguePlayVideo" {
-
             
+            // get destination view controller
+            let destVc = segue.destinationViewController as! AVPlayerViewController
+            
+            // create video url ( url added soon..)
+            let videoUrl = NSURL(string: "http://www.website.com/video-file.mov")
+            
+            // set player with url
+            destVc.player = AVPlayer(URL: videoUrl!)
         }
         
     }
